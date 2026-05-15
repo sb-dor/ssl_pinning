a9c7c360dc600a29dc13e9213f71b01dd9a3a1c6
sb-dor <avaz.shams.2002@gmail.com>
2026-05-15 23:54:01 +0500

    minor improvements

--- lib/http_client/http_client_io.dart
+++ lib/http_client/http_client_io.dart
@@ -4,6 +4,11 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:http/io_client.dart';
 
+///  To generate a binary certificate file
+/// 
+///  https://habr.com/ru/articles/504914/
+///  https://github.com/sb-dor/ssl_certificate_binary_generator
+
 Future<Client> httpClient() async {
   final sslCert = await rootBundle.load('assets/go_avera_cert.pem');
   final securityContext = SecurityContext(withTrustedRoots: false)
